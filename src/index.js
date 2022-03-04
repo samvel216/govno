28bf49ca9b2a105e36705d65ce56244c97eff801
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-03-04 13:43:32 +0200

    212

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+const faitListEl = document.querySelector(".fait-list");
+const imgCrushEl = document.querySelector(".img-crush");
+const faitListElOpaciti = () => {
+    imgCrushEl.style.opacity = "0"; 
+}
